12c407ad6b21c5f831fdabe76d4eda4dfc5db7ec
deepanshu414 <131351308+deepanshu414@users.noreply.github.com>
2024-02-25 15:57:47 +0530

    translator_python.py

--- translator_python.py
+++ translator_python.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from googletrans import Translator, LANGUAGES
-st.set_page_config(page_title="Translator",page_icon="https://static.vecteezy.com/system/resources/previews/022/663/262/non_2x/illustration-3d-chat-symbol-icon-talking-to-ai-robot-free-png.png")
+import requests
+st.set_page_config(page_title="Translator",page_icon="https://static.vecteezy.com/system/resources/previews/010/881/290/non_2x/language-conversation-translation-3d-icon-illustration-png.png")
 st.markdown("""
 <style>
 .st-at.st-au.st-av.st-aw.st-ae.st-ag.st-ah.st-af.st-c2.st-bo.st-c3.st-c4.st-c5.st-c6.st-am.st-an.st-ao.st-ap.st-c7.st-ar.st-as.st-bb.st-ax.st-ay.st-az.st-b0.st-b1 {
@@ -10,9 +10,6 @@ st.markdown("""
 }
 .appview-container.st-emotion-cache-1wrcr25.ea3mdgi4 {
     rigth:20%;            
-}
-button.styles_terminalButton__JBj5T {
-    display: none;
 }
 svg.st-cn.st-db.st-bb.st-dc.st-dd {
     display:none;
@@ -45,18 +42,32 @@ header.st-emotion-cache-18ni7ap.ezrtsby2 {
 }
 </style>
 """,True)
-l_list = list(LANGUAGES.values())
-def change(text, dest):
-    trans = Translator()
-    trans1 = trans.translate(text, dest=dest)
-    return trans1.text
+def main():
+    from googletrans import Translator, LANGUAGES
+    l_list = list(LANGUAGES.values())
 
-st.title("Translator")
-tex = st.text_area("", placeholder="Enter ...")
-len2 = st.selectbox("", l_list, key="2nd")
-bt = st.button("Translate")
-if bt:
-    if(tex!=""):
-        st.markdown(f"<textarea rows='3' style='caret-color: transparent;background-color:gainsboro;min-width:100%;max-width:100%;outline:none;border-radius:10px;padding:1em;margin:0;' readonly >{change(tex, len2).capitalize()}</textarea>",True)
-    else:
-        st.warning("Enter some text ...")
+    def change(text, dest):
+        trans = Translator()
+        trans1 = trans.translate(text, dest=dest)
+        return trans1.text
+
+    st.title("Translator")
+    tex = st.text_area("", placeholder="Enter ...")
+    len2 = st.selectbox("", l_list, key="2nd")
+    bt = st.button("Translate")
+     if bt:
+        if(tex!=""):
+            st.markdown(f"<textarea rows='3' style='caret-color: transparent;background-color:gainsboro;min-width:100%;max-width:100%;outline:none;border-radius:10px;padding:1em;margin:0;' readonly >{change(tex, len2).capitalize()}</textarea>",True)
+        else:
+            st.warning("Enter some text ...")
+def check_internet_connection():
+    try:
+        response = requests.get("http://www.google.com", timeout=5)
+        if response.status_code == 200:
+            main()
+        else:
+            st.error("Check your internet connection ...")
+    except requests.ConnectionError:
+        st.error("Check your internet connection ...")
+check_internet_connection()
+
